2c322e11405ba46af735e606e950fbb5d2848108
umapotumahesh@0103 <umapotumahesh@gmail.com>
2026-01-30 15:03:34 +0530

    fix render

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 4000;
 // MongoDB connection
 const connectDB = async () => {
     try {
-        const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://umapotumahesh_db_user:CCerJDWCrB6DkHm1@cluster0.6pgbnqw.mongodb.net/?appName=Cluster0';
+        const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://umapotumahesh_db_user:<basicuser03>@cluster0.6pgbnqw.mongodb.net/?appName=Cluster0';
         await mongoose.connect(mongoURI);
         console.log('MongoDB connected successfully');
     } catch (error) {
